77115655cd78ea95754b1156c3d9b1a997faa2b6
Joshua Lehman <joshlehman.dev@gmail.com>
2023-07-15 20:49:58 -0400

    enums

--- app.ts
+++ app.ts
@@ -6,20 +6,25 @@
 //   age: 30,
 // };
 
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN = "ADMIN",
+  READ_ONLY = 100,
+  AUTHOR = 200,
+}
+
+const person = {
   name: "Josh",
   age: 30,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
-person.role.push("Admin");
-person.role[0] = 10;
+// person.role.push("Admin");
+// person.role[0] = 10;
 
 let favoriteActivities: string[];
 
@@ -29,4 +34,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log("Is Admin");
 }
