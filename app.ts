5982e9c81f4141eec29d232d33397cc948d420a9
Joshua Lehman <joshlehman.dev@gmail.com>
2023-07-15 21:51:45 -0400

    void

--- app.ts
+++ app.ts
@@ -1,32 +1,15 @@
-function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-text"
-) {
-  let result;
-
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  //   if (resultConversion == "as-number") {
-  //     return +result;
-  //   } else return result.toString();
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, "as-number");
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
 
-console.log(combinedAges);
+printResult(add(5, 12));
 
-const combinedStringAges = combine("30", "26", "as-number");
+let combineValues;
 
-console.log(combinedStringAges);
+combineValues = add;
 
-const combineNames = combine("Max", "Anna", "as-text");
-
-console.log(combineNames);
+console.log(combineValues(8, 8));
